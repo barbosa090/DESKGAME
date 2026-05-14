4c69df85b71b44d1042d11d48a5a1c26bf923e1a
barbosa090 <gustavo.barbosa43@aluno.ce.gov.br>
2026-05-14 16:59:03 -0300

    Update data.php

--- src/config/data.php
+++ src/config/data.php
@@ -1,11 +1,26 @@
 <?php
-try{
-$con = new PDO("mysql:host=$host;dbname=$dbname", $root, $password);
 
+<?php
+$servename = "servename";
+$username="username";
+$password= "password";
+$dbase="mydb";
+
+try {
+$con = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "a conexão foi um sucesso.";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
 
-
-
+try {
+    $sql = "CREATE DATABASE mydb";
+    $con->exec($sql);
+    echo "database criada com sucesso";
+} catch(PDOException $e) {
+    echo "erro na criação " . $sql . $e->getMessage();
+}
 
 
 
