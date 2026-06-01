b4d9fbc4f560838191a672a9b2614b5b30879c54
barbosa090 <gustavo.barbosa43@aluno.ce.gov.br>
2026-06-01 11:55:14 -0300

    quinto commit eu acho

--- src/view/login.php
+++ src/view/login.php
@@ -1,11 +1,13 @@
 <?php
+require_once data.php
+$stmt = $pdo->query("SELECT * FROM usuarios");
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'] ?? '';
     $senha = $_POST['senha'] ?? '';
 
 }
     try{
-        $stmt = $pdo->prepare("SELECT id, senha FROM usuarios WHERE email = :email");
+        $stmt= PDO->prepare("SELECT id, senha FROM usuarios WHERE email = :email");
         $stmt->bindParam(':email', $email);
         $stmt->execute();
         $usuario = $stmt->fetch();
