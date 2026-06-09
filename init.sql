cc3a82786fa71f5f9e7084843d4325497ba90870
barbosa090 <gustavo.barbosa43@aluno.ce.gov.br>
2026-06-09 16:48:10 -0300

    Add files via upload

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE usuarios (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    sobrenome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
