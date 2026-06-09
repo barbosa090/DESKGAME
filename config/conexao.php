f0c7e473b914e695dc99424ea6062470d5fab297
barbosa090 <gustavo.barbosa43@aluno.ce.gov.br>
2026-06-08 22:06:55 -0300

    garantir o commit

--- config/conexao.php
+++ config/conexao.php
@@ -5,9 +5,9 @@ $usuario = 'root';
 $senha   = '';
 
 try {
-  $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $usuario, $senha);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  $conexaoPDO = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $usuario, $senha);
+  $conexaoPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conexaoPDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
   die("Erro ao conectar: " . $e->getMessage());
 }
