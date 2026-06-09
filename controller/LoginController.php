32321be060cef0e9d322b6ad7bbdaa1bb0380bcd
barbosa090 <gustavo.barbosa43@aluno.ce.gov.br>
2026-06-08 22:06:22 -0300

    php puro no controla mento do loginController

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -1,32 +1,20 @@
 <?php
 require_once __DIR__ . "/../config/conexao.php";
-
-class LoginController {
-    private $pdo;
-
-    
-    public function __construct($conexaoPDO) {
-        $this->pdo = $conexaoPDO; 
+function autenticarUsuario($pdo, $email, $senha) {
+    if (empty($email) || empty($senha)) {
+        return "Por favor, preencha todos os campos.";
     }
-
-    public function autenticar($email, $senha) {
-        if (empty($email) || empty($senha)) {
-            return "Por favor, preencha todos os campos.";
-        }
-
-        try {
-            $stmt = $this->pdo->prepare("INSERT INTO usuarios (email, senha) VALUES (:email, :senha)");
-            $stmt->bindParam(':email', $email);
-            $senhaSegura = password_hash($senha, PASSWORD_DEFAULT);
-            $stmt->bindParam(':senha', $senhaSegura);
-            $stmt->execute();
-            return true;
-        } catch (PDOException $e) {
-            return "Erro no banco de dados: " . $e->getMessage();
-        } catch (Exception $e) {
-            return "Erro geral: " . $e->getMessage();
-        }
-    
+    try {
+     $stmt = $pdo->prepare("INSERT INTO usuarios (email, senha) VALUES (:email, :senha)");
+     $stmt->bindParam(':email', $email); 
+    $senhaSegura = password_hash($senha, PASSWORD_DEFAULT);
+    $stmt->bindParam(':senha', $senhaSegura);
+        $stmt->execute();
+    return true;
+    } catch (PDOException $e) {
+        return "Erro no banco de dados: " . $e->getMessage();
+    } catch (Exception $e) {
+        return "Erro geral: " . $e->getMessage();
     }
 }
 ?>
