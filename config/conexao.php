91f986baecda5a08927256fd0edc730ce7254c89
barbosa090 <gustavo.barbosa43@aluno.ce.gov.br>
2026-06-11 00:13:06 -0300

    coloquei comentario dizendo que $host quando eu for rodar docker eu mudarei a variavel para colocar um $host = "db"
    conexaoPDO não coloquei $pdo normal

--- config/conexao.php
+++ config/conexao.php
@@ -1,13 +1,15 @@
 <?php
+// se precisar no host aponta exatamente para o nome do serviço do banco de dados que está no seu docker-compose.yml
+// e coloco "db"no $host
 $host    = 'localhost';
 $db      = 'deskgame';
 $usuario = 'root';
 $senha   = '';
 
 try {
-  $conexaoPDO = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $usuario, $senha);
-  $conexaoPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $conexaoPDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $usuario, $senha);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
   die("Erro ao conectar: " . $e->getMessage());
 }
