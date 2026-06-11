ec676d809f34aad29dce74f467dbd5d37ed439c8
barbosa090 <gustavo.barbosa43@aluno.ce.gov.br>
2026-06-11 00:01:51 -0300

    espaçamento aos codigos
    Updated the title and header color for the login page.

--- login.php
+++ login.php
@@ -1,20 +1,22 @@
 <?php
 require_once __DIR__ . '/config/conexao.php';
 
+
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
    $email =FILTER_INPUT(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
    $senha = $_POST['senha'];
    $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE email = ?");
    $stmt->execute([$email]);
     $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
     if ($usuario && password_verify($senha, $usuario['senha'])) {
         session_start();
         $_SESSION['logado'] = true;
         $_SESSION['id'] = $usuario['id'];
         $_SESSION['nome'] = $usuario['nome'];
         $_SESSION["tipo"] = $usuario['tipo'];
-
-       if($_SESSION["tipo"] === "admin") {
+    
+        if($_SESSION["tipo"] === "admin") {
             header("Location: admin.php");
             exit();
         } else {
@@ -31,12 +33,12 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Upgrade Gaming</title>
+    <meta name="viewport" content=""width=device-width, initial-scale=1.0">
+    <title>Login - DESKGAME</title>
     <link rel="stylesheet" href="login.css">
 </head>
 <body class="body-login"> <div class="login-container">
-        <h2>Upgrade Gaming</h2>
+        <h2 style="color: cyan;">DESKGAME</h2>
 
         <?php if (!empty($mensagemErro)): ?>
             <div class="erro-alerta">
