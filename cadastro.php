82971fe85f81694ed026fe2d203173d0a621afdd
barbosa090 <gustavo.barbosa43@aluno.ce.gov.br>
2026-06-11 00:08:05 -0300

    Corrigir a sintaxe do PHP e aprimorar a lógica de registro.

--- cadastro.php
+++ cadastro.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Criar Conta - DESKGAME</title>
     <link rel="stylesheet" href="cadastro.css">
-  
+    
 </head>
 <body>
     <div class="login-container">
